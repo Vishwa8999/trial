4ce1da010358ae2d17910f6f0e1e276e6c584ada
Vishwa8999 <164845227+Vishwa8999@users.noreply.github.com>
2024-09-20 15:16:03 +0530

    12

---
+++ hos script.js
@@ -0,0 +1,22 @@
+// JavaScript for Image Slider
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.display = (i === index) ? 'block' : 'none';
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+// Show the first slide by default
+showSlide(currentSlide);
